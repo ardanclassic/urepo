adc39dc0832e1c356fe63a16d6e09a0a13a1f3af
IBNSHADIQ\ardanclassic <akbar@glovory.com>
2021-08-21 12:16:38 +0700

    setup head & metadata information for repo page

--- pages/repository/[id].js
+++ pages/repository/[id].js
@@ -1,14 +1,22 @@
 import React from "react";
+import Head from "next/head";
 import RepoCard from "../../components/repositorybox/repocard";
 import UserCard from "../../components/repositorybox/usercard";
 import styles from "./repo.module.scss";
 
 const Repository = ({ id }) => {
   return (
-    <div className={styles.repoContainer}>
-      <UserCard id={id} />
-      <RepoCard id={id} />
-    </div>
+    <>
+      <Head>
+        <title>Urepo - Repository</title>
+        <meta name="description" content={`${id} detail & repository`} />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.repoContainer}>
+        <UserCard id={id} />
+        <RepoCard id={id} />
+      </div>
+    </>
   );
 };
 
